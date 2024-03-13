f1b51452fb9e427502198381b02d1afac855f0c7
Hayley Berry <hayley.berry@appreciategroup.co.uk>
2024-03-13 12:10:12 +0000

    Correct 'publishes' bug

--- src/NotificationsServiceProvider.php
+++ src/NotificationsServiceProvider.php
@@ -6,9 +6,12 @@ use Illuminate\Support\ServiceProvider as BaseServiceProvider;
 
 class NotificationsServiceProvider extends BaseServiceProvider
 {
-    public static array $publishes = [
-        __DIR__ . '/../config/notifications.php' => config_path('notifications.php'),
-    ];
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../config/notifications.php' => config_path('notifications.php'),
+        ], 'config');
+    }
 
     public function register(): void
     {
